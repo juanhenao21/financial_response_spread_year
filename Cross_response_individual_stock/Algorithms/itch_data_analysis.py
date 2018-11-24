5cfcaffe82d9f86ceb20c7e6e14e4c711258bc72
juanhenao21 <juanhenao21@gmail.com>
2018-11-24 17:29:02 +0100

    Adding comments to the ipynb code and creating the script in python

---
+++ Cross_response_individual_stock/Algorithms/itch_data_analysis.py
@@ -0,0 +1,29 @@
+'''
+Script to analyze the ITCH data with the information of 96 stocks during a week in 2016.
+
+'''
+
+def midpoint_data(ticker, day):
+    return None
+
+def midpoint_log_returns_data(ticker, day):
+    return None
+
+def midpoint_plot(ticker):
+    return None
+
+def trade_signs_data(ticker, day):
+    return None
+
+def cross_response_functions(ticker, day):
+    return None
+
+def main():
+    print('Ay vamos!!')
+    return None
+
+if __name__ == '__main__':
+    main()
+    
+
+
