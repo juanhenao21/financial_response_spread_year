9da5985e1d33d7885e4aa0429c1d50b8b0489e18
juanhenao21 <juanhenao21@gmail.com>
2019-02-11 16:19:12 +0100

    ITCH trade sign sel correlator data done

--- Cross_response_individual_stock/itch_algorithms/itch_data_generator.py
+++ Cross_response_individual_stock/itch_algorithms/itch_data_generator.py
@@ -947,7 +947,21 @@ def itch_avg_return_avg_trade_prod_data(ticker_i, ticker_j, year, month, day,
 
 def itch_difference_cross_response_avg_prod_data(ticker_i, ticker_j, year,
                                                  month, day, t_step):
-
+    """
+    Obtain the difference between the cross response function and the result
+    of the product between the averaged midpoint log return of ticker i and
+    the averaged trade signs of ticker j during different time
+    lags. The data is adjusted to use only the values each t_step ms
+        :param ticker_i: string of the abbreviation of the midpoint stock to
+         be analized (i.e. 'AAPL')
+        :param ticker_j: string of the abbreviation of the trade sign stock to
+         be analized (i.e. 'AAPL')
+        :param year: string of the year to be analized (i.e '2016')
+        :param month: string of the month to be analized (i.e '07')
+        :param day: string of the day to be analized (i.e '07')
+        :param tau_val: maximum time lag to be analyzed
+        :param t_step: time step in the data in ms
+    """
     if (ticker_i == ticker_j):
 
         return None
@@ -983,28 +997,34 @@ def itch_difference_cross_response_avg_prod_data(ticker_i, ticker_j, year,
 # -----------------------------------------------------------------------------------------------------------------------
 
 
-def itch_trade_sign_self_correlator_data(ticker_i, day, tau_val, t_step):
+def itch_trade_sign_self_correlator_data(ticker, year, month, day, tau_val,
+                                         t_step):
     """
     Obtain the trade sign self correlator using the trade signs of ticker i
     during different time lags. The data is adjusted to use only the values
     each t_step ms
         :param ticker_i: string of the abbreviation of the trade sign stock to
          be analized (i.e. 'AAPL')
+        :param year: string of the year to be analized (i.e '2016')
+        :param month: string of the month to be analized (i.e '07')
         :param day: string of the day to be analized (i.e '07')
         :param tau_val: maximum time lag to be analyzed
         :param t_step: time step in the data in ms
     """
-    print('trade sign cross correlator data')
-    print('Processing data for the stock i ' + ticker_i + ' the day ' + day
-          + ' March, 2016')
-    print('Time step: ', t_step, 'ms')
+    function_name = itch_trade_sign_self_correlator_data.__name__
+    itch_data_tools.itch_function_header_print_data(function_name, ticker,
+                                                    ticker, year, month,
+                                                    day, str(t_step))
 
     # Load data
-    trade_sign_i = pickle.load(open(''.join((
-                '../Data/trade_signs_data_1ms/trade_signs_data_201603{}'
-                + '_{}i_1ms.pickl').split())
-                .format(day, ticker_i), 'rb'))
-    time = pickle.load(open('../Data/midpoint_data/time.pickl', 'rb'))
+    trade_sign_i = pickle.load(open("".join((
+                '../itch_data_{1}/itch_trade_signs_data_1ms/itch_trade_signs'
+                + '_data_{1}{2}{3}_{0}_1ms.pickle').split())
+                .format(ticker, year, month, day), 'rb'))
+    time = pickle.load(open(''.join((
+            '../itch_data_{}/itch_midpoint_data_1ms/itch_midpoint_data'
+            + '_time_1ms.pickle').split())
+            .format(year), 'rb'))
 
     # Setting variables to work with t_step ms accuracy
 
@@ -1016,7 +1036,7 @@ def itch_trade_sign_self_correlator_data(ticker_i, day, tau_val, t_step):
 
     # reshape and average data of trade signs
     (trade_sign_i_sec_avg,
-     trade_sign_i_sec_nr) = itch_data_tools.trade_sign_reshape(
+     trade_sign_i_sec_nr) = itch_data_tools.itch_trade_sign_reshape(
         trade_sign_i, time_t_step)
 
     # Calculating the midpoint log return and the cross response function
@@ -1033,9 +1053,8 @@ def itch_trade_sign_self_correlator_data(ticker_i, day, tau_val, t_step):
 
     # Saving data
 
-    function_name = trade_sign_self_correlator_data.__name__
-    itch_data_tools.save_data(function_name, self_correlator, ticker_i,
-                              ticker_i, day, t_step)
+    itch_data_tools.itch_save_data(function_name, self_correlator, ticker,
+                                   ticker, year, month, day, str(t_step))
 
     return None
 
