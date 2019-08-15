f6531481a61c8fd60482f28cf4731362670369dd
juanhenao21 <juanhenao21@gmail.com>
2019-08-15 18:31:36 +0200

    Create docstrings for TAQ data tools article rep

--- project/taq_article_reproduction/taq_algorithms/taq_data_tools_article_reproduction.py
+++ project/taq_article_reproduction/taq_algorithms/taq_data_tools_article_reproduction.py
@@ -1,30 +1,43 @@
+''' TAQ data tools module.
+
+The functions in the module do small repetitive tasks, that are used along the
+whole implementation. These tools improve the way the tasks are standardized
+in the modules that use them.
+
+This script requires the following modules:
+    * numpy
+    * pandas
+    * matplotlib
+
+The module contains the following functions
+    * taq_save_data - saves computed data
+    * taq_save_plot - saves figures
+    * taq_function_header_print_data - prints info about the function running
+    * taq_function_header_print_plot - prints info about the plot
+    * taq_start_folders - creates folders to save data and plots
+    * taq_business_days - creates a list of week days for a year
+    * main - the main function of the script
+
+.. moduleauthor:: Juan Camilo Henao Londono <www.github.com/juanhenao21>
 '''
-TAQ data tools
 
-Module with functions that help to make more readable the modules.
-
-Juan Camilo Henao Londono
-juan.henao-londono@stud.uni-due.de
-'''
-
-# TO DO: Docstrings!
-
-# -----------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Modules
 
 from matplotlib import pyplot as plt
 import numpy as np
 import os
 import pandas as pd
-
 import pickle
 
-# -----------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 
 def taq_save_data(function_name, data, ticker_i, ticker_j, year, month, day):
-    """
-    Save the data generated in taq_data_analysis module.
+    """ Saves computed data in pickle files.
+
+    Saves the data generated in the functions of the
+    taq_data_analysis_article_reproduction module in pickle files.
         :param function_name: name of the function that generates the data
         :param data: python data to be saved
         :param ticker_i: string of the abbreviation of the stock to be analized
@@ -70,7 +83,7 @@ def taq_save_data(function_name, data, ticker_i, ticker_j, year, month, day):
 
     return None
 
-# -----------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 
 def taq_save_plot(function_name, figure, ticker_i, ticker_j, year, month):
@@ -117,7 +130,7 @@ def taq_save_plot(function_name, figure, ticker_i, ticker_j, year, month):
 
     return None
 
-# -----------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 
 def taq_function_header_print_data(function_name, ticker_i, ticker_j, year,
@@ -144,7 +157,7 @@ def taq_function_header_print_data(function_name, ticker_i, ticker_j, year,
 
     return None
 
-# -----------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 
 def taq_function_header_print_plot(function_name, ticker_i, ticker_j, year,
@@ -169,7 +182,7 @@ def taq_function_header_print_plot(function_name, ticker_i, ticker_j, year,
 
     return None
 
-# -----------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 
 def taq_start_folders(year):
@@ -194,56 +207,6 @@ def taq_start_folders(year):
         except FileExistsError:
 
             print('Folder exists. The folder was not created')
-
-# -----------------------------------------------------------------------------------------------------------------------
-
-
-def get_sec(time_str):
-    """
-    Convert time format from hh:mm:ss to seconds
-        :param time_str: string with the format hh:mm:ss (i. e. '09:40:00')
-    """
-    h, m, s = time_str.split(':')
-    return int(h) * 3600 + int(m) * 60 + int(s)
-
-# -----------------------------------------------------------------------------------------------------------------------
-
-
-def months_days_list(folder_path, ticker, year):
-    """
-    Generate two lists with the string with the numbers of the months
-    (from '01' to '12') and the days of the data to be analyzed.
-    """
-    days = []
-    days_list = []
-    months_list = []
-
-    for i in range(1, 32):
-        if (i < 10):
-            days.append('0' + str(i))
-        else:
-            days.append(str(i))
-
-    for m in range(1, 13):
-        if (m < 10):
-            months_list.append('0' + str(m))
-        else:
-            months_list.append(str(m))
-
-    files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
-
-    for month in months_list:
-        days_month = []
-        for d in days:
-            for file in files:
-                val_split = file.split('_')
-                date = val_split[-1].split('.')[0]
-                val = val_split[1] + val_split[2] + date
-                if (val == '{}quotes{}{}{}'.format(ticker, year, month, d)):
-                    days_month.append(d)
-        days_list += [days_month]
-
-    return(months_list, days_list)
 
 # -----------------------------------------------------------------------------
 
