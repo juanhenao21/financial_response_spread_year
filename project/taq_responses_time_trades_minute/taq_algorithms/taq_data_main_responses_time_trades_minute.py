0fe2ec5b22810abbe8d3c890f22cab0233423cd8
juanhenao21 <juanhenao21@gmail.com>
2019-08-26 19:21:15 +0200

    Review and document TAQ resp time trade minute main

--- project/taq_responses_time_trades_minute/taq_algorithms/taq_data_main_responses_time_trades_minute.py
+++ project/taq_responses_time_trades_minute/taq_algorithms/taq_data_main_responses_time_trades_minute.py
@@ -1,77 +1,111 @@
+'''TAQ data main module
+
+The functions in the module run the complete analysis and plot of the TAQ data.
+
+This script requires the following modules:
+    * itertools
+    * multiprocessing
+    * taq_data_analysis_responses_time_trades_minute
+    * taq_data_plot_responses_time_trades_minute
+    * taq_data_tools_responses_time_trades_minute
+
+The module contains the following functions:
+    * taq_data_plot_generator - generates all the analysis and plots from the
+      TAQ data.
+    * main - the main function of the script.
+
+.. moduleauthor:: Juan Camilo Henao Londono <www.github.com/juanhenao21>
 '''
-TAQ data main
 
-Script to analyze the TAQ data with the information of 6 stocks during a 2008.
-
-For the first proyect I need to obtain the cross response functions between
-individual stocks. To compute these values I need to calculate the midpoint
-price, the midpoint log returns and the trade signs. Each task is specified
-in each function.
-
-Juan Camilo Henao Londono
-juan.henao-londono@stud.uni-due.de
-'''
 # -----------------------------------------------------------------------------
 # Modules
 
 from itertools import product
-import subprocess
-
 import multiprocessing as mp
+import os
 import pickle
-import pandas as pd
 
-import taq_data_analysis
-import taq_data_plot
-import taq_data_tools
+import taq_data_analysis_responses_time_trades_minute
+import taq_data_plot_responses_time_trades_minute
+import taq_data_tools_responses_time_trades_minute
 
 __tau__ = 10000
+# -----------------------------------------------------------------------------
+
+
+def taq_data_plot_generator(tickers, year, taus):
+    """Generates all the analysis and plots from the TAQ data.
+
+    :param tickers: list of the string abbreviation of the stocks to be
+     analized (i.e. ['AAPL', 'MSFT']).
+    :param year: string of the year to be analized (i.e '2016').
+    :param taus: list of integers greater than zero (i.e. [1, 10, 50]).
+    :return: None -- The function saves the data in a file and does not return
+     a value.
+    """
+
+    # Self-response
+    self_parameters = product(tickers, [year], taus)
+    # Cross-response
+    cross_parameters = product(tickers, tickers, [year], taus)
+
+    for self_ in self_parameters:
+
+        taq_data_analysis_responses_time_trades_minute \
+            .taq_self_response_year_responses_time_trades_minute_data(*self_)
+        taq_data_analysis_responses_time_trades_minute \
+            .taq_self_response_year_avg_responses_time_trades_minute_data(
+                *self_)
+
+        taq_data_plot_responses_time_trades_minute \
+            .taq_self_response_year_responses_time_trades_minute_plot(*self_)
+
+    for cross in cross_parameters:
+
+        taq_data_analysis_responses_time_trades_minute \
+            .taq_cross_response_year_responses_time_trades_minute_data(*cross)
+        taq_data_analysis_responses_time_trades_minute \
+            .taq_cross_response_year_avg_responses_time_trades_minute_data(
+                *cross)
+
+        taq_data_plot_responses_time_trades_minute \
+            .taq_cross_response_year_responses_time_trades_minute_plot(*cross)
+
+    with mp.Pool(processes=mp.cpu_count()) as pool:
+        pool.starmap(
+            taq_data_plot_responses_time_trades_minute
+            .taq_self_response_year_avg_responses_time_trades_minute_plot,
+            product(tickers, [year], taus))
+        pool.starmap(
+            taq_data_plot_responses_time_trades_minute
+            .taq_cross_response_year_avg_responses_time_trades_minute_plot,
+            product(tickers, tickers, [year], taus))
+
+    return None
 
 # -----------------------------------------------------------------------------
 
 
 def main():
+    """The main function of the script.
+
+    The main function is used to test the functions in the script.
+
+    :return: None.
+    """
 
     # Tickers and days to analyze
-
-    tickers = ['AAPL', 'MSFT']
+    tickers = ['AAPL', 'CVX', 'GS', 'JPM', 'MSFT', 'XOM']
     year = '2008'
     taus = [5, 10, 50, 100, 500, 1000]
 
-    with mp.Pool(processes=mp.cpu_count()) as pool:
+    # Basic folders
+    taq_data_tools_responses_time_trades_minute.taq_start_folders('2008')
 
-        pool.starmap(taq_data_analysis.taq_self_response_year_responses_time_trades_minute_data,
-                     product(tickers, [year], taus))
+    # Run analysis
+    taq_data_plot_generator(tickers, year, taus)
 
-        pool.starmap(taq_data_analysis.taq_cross_response_year_responses_time_trades_minute_data,
-                     product(tickers, tickers, [year], taus))
-
-        pool.starmap(taq_data_plot.taq_self_response_year_responses_time_trades_minute_plot,
-                     product(tickers, [year], taus))
-        pool.starmap(taq_data_plot.taq_cross_response_year_responses_time_trades_minute_plot,
-                     product(tickers, tickers, [year], taus))
-
-        pool.starmap(taq_data_analysis.taq_self_response_year_avg_responses_time_trades_minute_data,
-                     product(tickers, [year], taus))
-        pool.starmap(taq_data_analysis.taq_cross_response_year_avg_responses_time_trades_minute_data,
-                     product(tickers, tickers, [year], taus))
-
-        pool.starmap(taq_data_plot.taq_self_response_year_avg_responses_time_trades_minute_plot,
-                     product(tickers, [year], taus))
-        pool.starmap(taq_data_plot.taq_cross_response_year_avg_responses_time_trades_minute_plot,
-                     product(tickers, tickers, [year], taus))
-
-        pool.starmap(taq_data_analysis.taq_self_response_year_avg_responses_time_trades_minute_data_v2,
-                     product(tickers, [year], taus))
-        pool.starmap(taq_data_analysis.taq_cross_response_year_avg_responses_time_trades_minute_data_v2,
-                     product(tickers, tickers, [year], taus))
-
-        pool.starmap(taq_data_plot.taq_self_response_year_avg_responses_time_trades_minute_plot_v2,
-                     product(tickers, [year], taus))
-        pool.starmap(taq_data_plot.taq_cross_response_year_avg_responses_time_trades_minute_plot_v2,
-                     product(tickers, tickers, [year], taus))
-
-        print('Ay vamos!!')
+    print('Ay vamos!!')
 
     return None
 
