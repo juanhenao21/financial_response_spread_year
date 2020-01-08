a2b1233f9cbc9688756e39cfa41edd1a767fc7a3
juanhenao21 <juanhenao21@gmail.com>
2020-01-08 08:35:37 +0100

    Update TAQ data tools responses second module

--- project/taq_responses_second/taq_algorithms/taq_data_tools_responses_second.py
+++ project/taq_responses_second/taq_algorithms/taq_data_tools_responses_second.py
@@ -7,7 +7,10 @@ in the modules that use them.
 This script requires the following modules:
     * matplotlib
     * numpy
+    * os
     * pandas
+    * pickle
+    * subprocess
 
 The module contains the following functions:
     * taq_save_data - saves computed data.
@@ -39,7 +42,7 @@ def taq_save_data(function_name, data, ticker_i, ticker_j, year, month, day):
     """ Saves computed data in pickle files.
 
     Saves the data generated in the functions of the
-    taq_data_analysis_article_reproduction module in pickle files.
+    taq_data_analysis_responses_second module in pickle files.
 
     :param function_name: name of the function that generates the data.
     :param data: data to be saved. The data can be of different types.
@@ -50,18 +53,18 @@ def taq_save_data(function_name, data, ticker_i, ticker_j, year, month, day):
     :param year: string of the year to be analized (i.e '2016').
     :param month: string of the month to be analized (i.e '07').
     :param day: string of the day to be analized (i.e '07').
-    :return: None -- The function saves the data in a file and does not return a
-     value.
+    :return: None -- The function saves the data in a file and does not return
+     a value.
     """
 
     # Saving data
 
-    if (not os.path.isdir('../../taq_data/article_reproduction_data_{1}/{0}/'
-                          .format(function_name, year))):
+    if (not os.path.isdir(
+            f'../../taq_data/responses_second_data_{year}/{function_name}/')):
 
         try:
-            os.mkdir('../../taq_data/article_reproduction_data_{1}/{0}/'
-                     .format(function_name, year))
+            os.mkdir(f'../../taq_data/responses_second_data_{year}/'
+                     + f'{function_name}/')
             print('Folder to save data created')
 
         except FileExistsError:
@@ -70,19 +73,16 @@ def taq_save_data(function_name, data, ticker_i, ticker_j, year, month, day):
     # Cross-response data
     if (ticker_i != ticker_j):
 
-        pickle.dump(data, open(''.join((
-            '../../taq_data/article_reproduction_data_{3}/{0}/{0}_{3}{4}{5}'
-            + '_{1}i_{2}j.pickle').split())
-            .format(function_name, ticker_i, ticker_j, year, month, day),
-            'wb'))
+        pickle.dump(data, open(f'../../taq_data/responses_second_data_{year}/'
+                    + f'{function_name}/{function}_{year}{month}{day}'
+                    + f'_{ticker_i}i_{ticker_j}j.pickle', 'wb'))
 
     # Self-response data
     else:
 
-        pickle.dump(data, open(''.join((
-            '../../taq_data/article_reproduction_data_{2}/{0}/{0}_{2}{3}{4}'
-            '_{1}.pickle').split())
-            .format(function_name, ticker_i, year, month, day), 'wb'))
+        pickle.dump(data, open(f'../../taq_data/responses_second_data_{year}/'
+                    + f'{function_name}/{function_name}_{year}{month}{day}'
+                    + f'_{ticker_i}.pickle', 'wb'))
 
     print('Data Saved')
     print()
@@ -96,7 +96,7 @@ def taq_save_plot(function_name, figure, ticker_i, ticker_j, year, month):
     """Saves plot in png files.
 
     Saves the plot generated in the functions of the
-    taq_data_plot_article_reproduction module in png files.
+    taq_data_plot_responses_second module in png files.
 
     :param function_name: name of the function that generates the plot.
     :param figure: figure object that is going to be save.
@@ -112,12 +112,12 @@ def taq_save_plot(function_name, figure, ticker_i, ticker_j, year, month):
 
     # Saving plot data
 
-    if (not os.path.isdir('../../taq_plot/article_reproduction_plot_{1}/{0}/'
-                          .format(function_name, year))):
+    if (not os.path.isdir(
+            f'../../taq_plot/responses_second_plot_{year}/{function_name}/')):
 
         try:
-            os.mkdir('../../taq_plot/article_reproduction_plot_{1}/{0}/'
-                     .format(function_name, year))
+            os.mkdir(f'../../taq_plot/responses_second_plot_{year}/'
+                     + f'{function_name}/')
             print('Folder to save data created')
 
         except FileExistsError:
@@ -126,18 +126,16 @@ def taq_save_plot(function_name, figure, ticker_i, ticker_j, year, month):
     # Cross-response data
     if (ticker_i != ticker_j):
 
-        figure.savefig(''.join((
-            '../../taq_plot/article_reproduction_plot_{3}/{0}/{0}_{3}{4}_{1}i'
-            + '_{2}j.png').split())
-            .format(function_name, ticker_i, ticker_j, year, month))
+        figure.savefig(f'../../taq_plot/responses_second_plot_{year}/'
+                       + f'{function_name}/{function_name}_{year}{month}'
+                       + f'_{ticker_i}i_{ticker_j}j.png')
 
     # Self-response
     else:
 
-        figure.savefig(''.join((
-            '../../taq_plot/article_reproduction_plot_{2}/{0}/{0}_{2}{3}_{1}i'
-            + '.png').split())
-            .format(function_name, ticker_i, year, month))
+        figure.savefig(f'../../taq_plot/responses_second_plot_{year}/'
+                       + f'{function_name}/{function_name}_{year}{month}'
+                       + f'_{ticker_i}i.png')
 
     print('Plot saved')
     print()
@@ -168,12 +166,12 @@ def taq_function_header_print_data(function_name, ticker_i, ticker_j, year,
 
     # Cross-response data
     if (ticker_i != ticker_j):
-        print('Processing data for the stock i ' + ticker_i + ' and stock j '
-              + ticker_j + ' the ' + year + '.' + month + '.' + day)
+        print(f'Processing data for the stock i {ticker_i} and stock j '
+              + f'{ticker_j} the {year}.{month}.{day}')
     # Self-response data
     else:
-        print('Processing data for the stock ' + ticker_i + ' the ' + year
-              + '.' + month + '.' + day)
+        print(f'Processing data for the stock {ticker_i} the '
+              + f'{year}.{month}.{day}')
 
     return None
 
@@ -201,12 +199,12 @@ def taq_function_header_print_plot(function_name, ticker_i, ticker_j, year,
 
     # Cross-response data
     if (ticker_i != ticker_j):
-        print('Processing plot for the stock i ' + ticker_i + ' and stock j '
-              + ticker_j + ' the ' + year + '.' + month + '.' + day)
+        print(f'Processing plot for the stock i {ticker_i} and stock j '
+              + f'{ticker_j} the {year}.{month}.{day}')
     # Self-response data
     else:
-        print('Processing plot for the stock ' + ticker_i + ' the ' + year
-              + '.' + month + '.' + day)
+        print(f'Processing plot for the stock {ticker_i} the '
+              + f'{year}.{month}.{day}')
 
     return None
 
@@ -221,13 +219,11 @@ def taq_start_folders(year):
     """
 
     try:
-        os.mkdir('../../taq_data/csv_year_data_{}'.format(year))
-        os.mkdir('../../taq_data/pickle_dayly_data_{}'.format(year))
+        os.mkdir(f'../../taq_data/csv_year_data_{year}')
+        os.mkdir(f'../../taq_data/hdf5_dayly_data_{year}')
         os.mkdir('../../taq_plot/')
-        os.mkdir('../../taq_data/article_reproduction_data_{}'
-                    .format(year))
-        os.mkdir('../../taq_plot/article_reproduction_plot_{}'
-                    .format(year))
+        os.mkdir(f'../../taq_data/responses_second_data_{year}')
+        os.mkdir(f'../../taq_plot/responses_second_plot_{year}')
         print('Folder to save data created')
 
     except FileExistsError as e:
@@ -247,8 +243,8 @@ def taq_bussiness_days(year):
     :return: list.
     """
 
-    init_date = '01/01/{}'.format(year)
-    last_date = '12/31/{}'.format(year)
+    init_date = f'01/01/{year}'
+    last_date = f'12/31/{year}'
 
     # Use only the bussiness days
     dt = pd.date_range(start=init_date, end=last_date, freq='B')
@@ -271,20 +267,19 @@ def taq_decompress(ticker, year, type):
     """
 
     if (type == 'quotes'):
-        subprocess.call('./decompress.out {}_{}_NASDAQ.quotes'
-                        .format(ticker, year) +
-                        ' > {}_{}_NASDAQ_quotes.csv'
-                        .format(ticker, year),
+        subprocess.call(f'./decompress.out {ticker}_{year}_NASDAQ.quotes'
+                        + f' > {ticker}_{year}_NASDAQ_quotes.csv',
                         shell=True, stdout=subprocess.PIPE)
 
     elif (type == 'trades'):
-        subprocess.call('./decompress.out {}_{}_NASDAQ.trades'
-                        .format(ticker, year) +
-                        ' > {}_{}_NASDAQ_trades.csv'
-                        .format(ticker, year),
+        subprocess.call(f'./decompress.out {ticker}_{year}_NASDAQ.trades'
+                        + f' > {ticker}_{year}_NASDAQ_trades.csv',
                         shell=True, stdout=subprocess.PIPE)
 
     return None
+
+# -----------------------------------------------------------------------------
+
 
 def main():
     """The main function of the script.
