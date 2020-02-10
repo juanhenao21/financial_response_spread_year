04cbb55227538118e344880f81a5d978e5f4c786
juanhenao21 <juanhenao21@gmail.com>
2020-02-10 08:24:23 +0100

    Update TAQ tools responses physical shift

--- project/taq_responses_physical_shift/taq_algorithms/taq_data_tools_responses_physical_shift.py
+++ project/taq_responses_physical_shift/taq_algorithms/taq_data_tools_responses_physical_shift.py
@@ -7,7 +7,9 @@ in the modules that use them.
 This script requires the following modules:
     * matplotlib
     * numpy
+    * os
     * pandas
+    * pickle
 
 The module contains the following functions:
     * taq_save_data - saves computed data.
@@ -15,6 +17,7 @@ The module contains the following functions:
     * taq_function_header_print_data - prints info about the function running.
     * taq_function_header_print_plot - prints info about the plot.
     * taq_start_folders - creates folders to save data and plots.
+    * taq_initial_data - takes the initial values for the analysis.
     * taq_business_days - creates a list of week days for a year.
     * main - the main function of the script.
 
@@ -37,7 +40,7 @@ def taq_save_data(function_name, data, ticker_i, ticker_j, year, month, day):
     """ Saves computed data in pickle files.
 
     Saves the data generated in the functions of the
-    taq_data_analysis_responses_time_shift module in pickle files.
+    taq_data_analysis_responses_physical_shift module in pickle files.
 
     :param function_name: name of the function that generates the data.
     :param data: data to be saved. The data can be of different types.
@@ -54,12 +57,12 @@ def taq_save_data(function_name, data, ticker_i, ticker_j, year, month, day):
 
     # Saving data
 
-    if (not os.path.isdir('../../taq_data/responses_time_shift_data_{1}/{0}/'
-                          .format(function_name, year))):
+    if (not os.path.isdir(f'../../taq_data/responses_physical_shift_data'
+                          + f'_{year}/{function_name}/')):
 
         try:
-            os.mkdir('../../taq_data/responses_time_shift_data_{1}/{0}/'
-                     .format(function_name, year))
+            os.mkdir(f'../../taq_data/responses_physical_shift_data_{year}/'
+                     + f'{function_name}/')
             print('Folder to save data created')
 
         except FileExistsError:
@@ -68,19 +71,18 @@ def taq_save_data(function_name, data, ticker_i, ticker_j, year, month, day):
     # Cross-response data
     if (ticker_i != ticker_j):
 
-        pickle.dump(data, open(''.join((
-            '../../taq_data/responses_time_shift_data_{3}/{0}/{0}_{3}{4}{5}'
-            + '_{1}i_{2}j.pickle').split())
-            .format(function_name, ticker_i, ticker_j, year, month, day),
-            'wb'))
+        pickle.dump(data, open(
+            f'../../taq_data/responses_physical_shift_data_{year}/'
+            + f'{function_name}/{function_name}_{year}{month}{day}_{ticker_i}i'
+            + f'_{ticker_j}j.pickle', 'wb'))
 
     # Self-response data
     else:
 
-        pickle.dump(data, open(''.join((
-            '../../taq_data/responses_time_shift_data_{2}/{0}/{0}_{2}{3}{4}'
-            '_{1}.pickle').split())
-            .format(function_name, ticker_i, year, month, day), 'wb'))
+        pickle.dump(data, open(
+            f'../../taq_data/responses_physical_shift_data_{year}/'
+            + f'{function_name}/{function_name}_{year}{month}{day}_{ticker_i}'
+            + f'.pickle', 'wb'))
 
     print('Data Saved')
     print()
@@ -94,7 +96,7 @@ def taq_save_plot(function_name, figure, ticker_i, ticker_j, year, month):
     """Saves plot in png files.
 
     Saves the plot generated in the functions of the
-    taq_data_plot_responses_time_shift module in png files.
+    taq_data_plot_responses_physical_shift module in png files.
 
     :param function_name: name of the function that generates the plot.
     :param figure: figure object that is going to be save.
@@ -110,12 +112,12 @@ def taq_save_plot(function_name, figure, ticker_i, ticker_j, year, month):
 
     # Saving plot
 
-    if (not os.path.isdir('../../taq_plot/responses_time_shift_plot_{1}/{0}/'
-                          .format(function_name, year))):
+    if (not os.path.isdir(f'../../taq_plot/responses_physical_shift_plot'
+                          + f'_{year}/{function_name}/')):
 
         try:
-            os.mkdir('../../taq_plot/responses_time_shift_plot_{1}/{0}/'
-                     .format(function_name, year))
+            os.mkdir(f'../../taq_plot/responses_physical_shift_plot_{year}/'
+                     + f'{function_name}/')
             print('Folder to save data created')
 
         except FileExistsError:
@@ -124,18 +126,16 @@ def taq_save_plot(function_name, figure, ticker_i, ticker_j, year, month):
     # Cross-response data
     if (ticker_i != ticker_j):
 
-        figure.savefig(''.join((
-                       '../../taq_plot/responses_time_shift_plot_{3}/{0}/{0}'
-                       + '_{3}{4}_{1}i_{2}j.png').split())
-                       .format(function_name, ticker_i, ticker_j, year, month))
+        figure.savefig(f'../../taq_plot/responses_physical_shift_plot_{year}'
+                       + f'/{function_name}/{function_name}_{year}{month}'
+                       + f'_{ticker_i}i_{ticker_j}j.png')
 
     # Self-response data
     else:
 
-        figure.savefig(''.join((
-                       '../../taq_plot/responses_time_shift_plot_{2}/{0}/{0}'
-                       + '_{2}{3}_{1}.png').split())
-                       .format(function_name, ticker_i, year, month))
+        figure.savefig(f'../../taq_plot/responses_physical_shift_plot_{year}'
+                       + f'/{function_name}/{function_name}_{year}{month}'
+                       + f'_{ticker_i}.png')
 
     print('Plot saved')
     print()
@@ -166,12 +166,12 @@ def taq_function_header_print_data(function_name, ticker_i, ticker_j, year,
 
     # Cross-response data
     if (ticker_i != ticker_j):
-        print('Processing data for the stock i ' + ticker_i + ' and stock j '
-              + ticker_j + ' the ' + year + '.' + month + '.' + day)
+        print(f'Processing data for the stock i {ticker_i} and stock j '
+              + f'{ticker_j} the {year}.{month}.{day}')
     # Self-response data
     else:
-        print('Processing data for the stock ' + ticker_i + ' the ' + year
-              + '.' + month + '.' + day)
+        print(f'Processing data for the stock {ticker_i} the '
+              + f'{year}.{month}.{day}')
 
     return None
 
@@ -199,12 +199,12 @@ def taq_function_header_print_plot(function_name, ticker_i, ticker_j, year,
 
     # Cross-response data
     if (ticker_i != ticker_j):
-        print('Processing plot for the stock i ' + ticker_i + ' and stock j '
-              + ticker_j + ' the ' + year + '.' + month + '.' + day)
+        print(f'Processing plot for the stock i {ticker_i} and stock j '
+              + f'{ticker_j} the {year}.{month}.{day}')
     # Self-response data
     else:
-        print('Processing plot for the stock ' + ticker_i + ' the ' + year
-              + '.' + month + '.' + day)
+        print(f'Processing plot for the stock {ticker_i} the '
+              + f'{year}.{month}.{day}')
 
     return None
 
@@ -219,10 +219,8 @@ def taq_start_folders(year):
     """
 
     try:
-        os.mkdir('../../taq_data/responses_time_shift_data_{}'
-                 .format(year))
-        os.mkdir('../../taq_plot/responses_time_shift_plot_{}'
-                 .format(year))
+        os.mkdir(f'../../taq_data/responses_physical_shift_data_{year}')
+        os.mkdir(f'../../taq_plot/responses_physical_shift_plot_{year}')
         print('Folder to save data created')
 
     except FileExistsError as e:
@@ -235,6 +233,60 @@ def taq_start_folders(year):
 # -----------------------------------------------------------------------------
 
 
+def taq_initial_data():
+    """Takes the initial values for the analysis
+
+    :return: Tuple -- The function return a tuple with a string with the year
+     to be analized and a list with the name of the tickers.
+    """
+
+    print()
+    print('###############################################')
+    print('Response Functions Physical Time Shift Analysis')
+    print('###############################################')
+    print('AG Guhr')
+    print('Faculty of Physics')
+    print('University of Duisburg-Essen')
+    print('Author: Juan Camilo Henao Londono')
+    print('More information in:')
+    print('  * https://juanhenao21.github.io/')
+    print('  * https://github.com/juanhenao21/response_functions_year')
+    print()
+
+    tickers = ['AAPL', 'MSFT', 'GS', 'JPM', 'XOM', 'CVX']
+
+    for ticker in tickers[:]:
+
+        print(f'Do you want to use the {ticker} ticker? (yes/no)')
+        res = input()
+
+        if (res == 'no'):
+            tickers.remove(ticker)
+
+    print()
+
+    shifts = []
+    cond = True
+    while (cond):
+        print('Do you want to add a shift value? (yes/no)')
+        res = input()
+
+        if (res == "yes"):
+            print('Add the value')
+            shifts.append(int(input()))
+        else:
+            cond = False
+
+    print()
+    print('Please enter the year to be analyzed (i.e. 2008): ')
+    year = input()
+    print()
+
+    return (year, tickers, taus)
+
+# -----------------------------------------------------------------------------
+
+
 def taq_bussiness_days(year):
     """Generates a list with the dates of the bussiness days in a year
 
@@ -242,8 +294,8 @@ def taq_bussiness_days(year):
     :return: list.
     """
 
-    init_date = '01/01/{}'.format(year)
-    last_date = '12/31/{}'.format(year)
+    init_date = f'01/01/{year}'
+    last_date = f'12/31/{year}'
 
     # Use only the bussiness days
     dt = pd.date_range(start=init_date, end=last_date, freq='B')
