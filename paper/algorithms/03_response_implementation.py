2a0490af604a225aa01467236c34cc9ceca7a443
juanhenao21 <juanhenao21@gmail.com>
2020-02-18 11:07:09 +0100

    Add docstrings

--- paper/algorithms/03_response_implementation.py
+++ paper/algorithms/03_response_implementation.py
@@ -1,17 +1,18 @@
-'''TAQ response comparison.
+'''TAQ response implementation.
 
-Plot the results of the response of different scales in the same figure to
-compare.
+Plots the figures of the response implementation for the paper.
 
 This script requires the following modules:
     * matplotlib
-    * numpy
+    * pickle
 
 The module contains the following functions:
-    * taq_self_response_year_avg_plot - plots the self-response average for a
-      year.
-    * taq_cross_response_year_avg_plot - plots the cross-response average for a
-      year.
+    * taq_trade_scale_response_year_avg_plot - plots the average response for a
+      year in trade time scale.
+    * taq_physical_scale_response_year_avg_plot - plots the average response
+      for a year in physical time scale.
+    * taq_response_year_avg_comparison_plot - plots the average response for a
+      year in trade time scale, physical time scale and the activity response.
     * main - the main function of the script.
 
 .. moduleauthor:: Juan Camilo Henao Londono <www.github.com/juanhenao21>
@@ -49,8 +50,8 @@ def taq_trade_scale_response_year_avg_plot(tickers, sectors, year):
             # Load data
             self_resp = pickle.load(open(
                 f'../../project/taq_data/responses_trade_data_{year}/taq_self'
-                + f'_response_year_responses_trade_data/taq_self_response'
-                + f'_year_responses_trade_data_{year}_{ticker_i}.pickle','rb'))
+                + f'_response_year_responses_trade_data/taq_self_response_year'
+                + f'_responses_trade_data_{year}_{ticker_i}.pickle', 'rb'))
 
             ax1.semilogx(self_resp, linewidth=5, label=f'{ticker_i}')
 
@@ -186,11 +187,12 @@ def taq_physical_scale_response_year_avg_plot(tickers, sectors, year):
 # ----------------------------------------------------------------------------
 
 
-
 def taq_response_year_avg_comparison_plot(ticker_i, ticker_j, year):
-    """Plots the comparison of self-response for different time scales.
+    """Plots the comparison of responses for different time scales.
 
-    :param ticker: string of the abbreviation of the stock to be analized
+    :param ticker_i: string of the abbreviation of the stock to be analized
+     (i.e. 'AAPL').
+    :param ticker_j: string of the abbreviation of the stock to be analized
      (i.e. 'AAPL').
     :param year: string of the year to be analized (i.e '2008').
     :return: None -- The function saves the plot in a file and does not return
@@ -201,7 +203,6 @@ def taq_response_year_avg_comparison_plot(ticker_i, ticker_j, year):
         figure = plt.figure(figsize=(16, 6))
         ax1 = plt.subplot(1, 2, 1)
         ax2 = plt.subplot(1, 2, 2)
-
 
         # Load data
         self_physical = pickle.load(open(
@@ -226,7 +227,7 @@ def taq_response_year_avg_comparison_plot(ticker_i, ticker_j, year):
         ax1.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2), ncol=3,
                    fontsize=15)
         ax1.set_xlabel(r'$\tau \, [s]$', fontsize=15)
-        ax1.set_ylabel(r'$R_{ii}(\tau)$ %s' %(ticker_i), fontsize=15)
+        ax1.set_ylabel(r'$R_{ii}(\tau)$ %s' % (ticker_i), fontsize=15)
         ax1.tick_params(axis='x', labelsize=10)
         ax1.tick_params(axis='y', labelsize=10)
         ax1.set_xlim(1, 1000)
@@ -261,7 +262,7 @@ def taq_response_year_avg_comparison_plot(ticker_i, ticker_j, year):
         ax2.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2), ncol=3,
                    fontsize=15)
         ax2.set_xlabel(r'$\tau \, [s]$', fontsize=15)
-        ax2.set_ylabel(r'$R_{ij}(\tau)$ %s - %s' %(ticker_i, ticker_j),
+        ax2.set_ylabel(r'$R_{ij}(\tau)$ %s - %s' % (ticker_i, ticker_j),
                        fontsize=15)
         ax2.tick_params(axis='x', labelsize=10)
         ax2.tick_params(axis='y', labelsize=10)
@@ -304,9 +305,9 @@ def main():
                ['GS', 'JPM'], ['JPM', 'GS']]
     year = '2008'
 
-    taq_response_year_avg_comparison_plot(ticker_i, ticker_j, year)
     taq_trade_scale_response_year_avg_plot(tickers, sectors, year)
     taq_physical_scale_response_year_avg_plot(tickers, sectors, year)
+    taq_response_year_avg_comparison_plot(ticker_i, ticker_j, year)
 
     return None
 
