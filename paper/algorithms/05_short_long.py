473fa27bfe5032d1902a824ab5cd8ca3956c25ea
juanhenao21 <juanhenao21@gmail.com>
2020-02-19 16:58:02 +0100

    Update paper short long figure

--- paper/algorithms/05_short_long.py
+++ paper/algorithms/05_short_long.py
@@ -1,100 +1,39 @@
-'''Short and long self- and cross responses figures
+'''TAQ short and long self- and cross-responses figures
 
-Plot the short and long self- and cross responses for the stocks AAPL, CVX,
-GS, JPM, MSFT, and XOM.
+Plots the figures of the short and long self- and cross-responses for the paper
+
+This script requires the following modules:
+    * matplotlib
+    * numpy
+    * pickle
+
+The module contains the following functions:
+    * taq_responses_physical_short_long_year_plot - plots the short and long
+      response for a year in physical physical scale.
+    * main - the main function of the script.
+
+.. moduleauthor:: Juan Camilo Henao Londono <www.github.com/juanhenao21>
 '''
 
 # ----------------------------------------------------------------------------
 # Modules
+
 from matplotlib import pyplot as plt
 import numpy as np
-import os
 import pickle
 
 # ----------------------------------------------------------------------------
 
 
-def taq_self_response_year_avg_responses_time_short_long_plot(ticker, year,
-                                                              tau, tau_p):
-    """Plots the short and long self-response average for a year.
-
-    :param ticker: string of the abbreviation of the stock to be analized
-     (i.e. 'AAPL').
-    :param year: string of the year to be analized (i.e '2008').
-    :param tau: integer greater than zero (i.e. 50).
-    :param tau_p: integer greater than zero and smaller than tau (i.e. 10).
-    :return: None -- The function saves the plot in a file and does not return
-     a value.
-    """
-
-    try:
-        figure = plt.figure(figsize=(16, 9))
-        ax = figure.add_subplot(111)
-
-        # Load data
-        (self_short,
-         self_long,
-         self_response,
-         self_shuffle) = pickle.load(open(''.join((
-                                     '../../project/taq_data/responses_time'
-                                     + '_short_long_data_{1}/taq_self_response'
-                                     + '_year_time_short_long_tau_data_tau'
-                                     + '_{2}_tau_p_{3}/taq_self_response_year'
-                                     + '_time_short_long_tau_data_tau_{2}_tau'
-                                     + '_p_{3}_{1}_{0}.pickle').split())
-                                     .format(ticker, year, tau, tau_p), 'rb'))
-
-        # Addition of the short and long response signal
-        sum = np.zeros(tau)
-        sum[:tau_p + 1] = self_short[:tau_p + 1]
-        sum[tau_p + 1:] = self_short[tau_p + 1:] + self_long[tau_p + 1:]
-
-        plt.semilogx(self_short, linewidth=5, label='Short')
-        plt.semilogx(self_long, linewidth=5, label='Long')
-        plt.semilogx(sum, linewidth=5, label='Sum')
-        plt.semilogx(self_response, linewidth=5, label='Self-response')
-        plt.semilogx(self_shuffle, linewidth=5, label='Shuffle')
-        plt.plot((tau_p, tau_p), (-1000, 1000), '--k', linewidth=5,
-                 label=r"$\tau' $ = {}".format(tau_p))
-
-        plt.legend(loc='lower left', fontsize=35)
-        # plt.title('Self-response', fontsize=40)
-        plt.xlabel(r'$\tau \, [s]$', fontsize=45)
-        plt.ylabel(r'$R_{ii}(\tau)$', fontsize=45)
-        plt.xticks(fontsize=35)
-        plt.yticks(fontsize=35)
-        plt.xlim(1, 1000)
-        plt.ylim(-0.2 * 10 ** -4, 1.6 * 10 ** -4)
-        plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
-        ax.yaxis.offsetText.set_fontsize(35)
-        plt.grid(True)
-        plt.tight_layout()
-        plt.show()
-
-        # Save plot
-        figure.savefig('../plot/05_self_short_long_{}.png'.format(ticker))
-
-        return None
-
-    except FileNotFoundError as e:
-        print('No data')
-        print(e)
-        print()
-        return None
-
-# ----------------------------------------------------------------------------
-
-
-def taq_cross_response_year_avg_responses_time_short_long_plot(ticker_i,
-                                                               ticker_j, year,
-                                                               tau, tau_p):
-    """Plots the short and long cross-response average for a year.
+def taq_responses_physical_short_long_year_plot(ticker_i, ticker_j, year, tau,
+                                                tau_p):
+    """Plots the short and long response for a year.
 
     :param ticker_i: string of the abbreviation of the stock to be analized
      (i.e. 'AAPL')
     :param ticker_j: string of the abbreviation of the stock to be analized
      (i.e. 'AAPL')
-    :param year: string of the year to be analized (i.e '2008')
+    :param year: string of the year to be analized (i.e '2008').
     :param tau: integer greater than zero (i.e. 50).
     :param tau_p: integer greater than zero and smaller than tau (i.e. 10).
     :return: None -- The function saves the plot in a file and does not return
@@ -107,56 +46,88 @@ def taq_cross_response_year_avg_responses_time_short_long_plot(ticker_i,
         return None
 
     else:
-        try:
-            figure = plt.figure(figsize=(16, 9))
-            ax = figure.add_subplot(111)
 
-            # Load data
+        try:
+            figure = plt.figure(figsize=(16, 6))
+            ax1 = plt.subplot(1, 2, 1)
+            ax2 = plt.subplot(1, 2, 2)
+
+            # Load data self-response
+            (self_short,
+            self_long,
+            self_response,
+            self_shuffle) = pickle.load(open(
+                f'../../project/taq_data/responses_physical_short_long_data'
+                + f'_{year}/taq_self_response_year_responses_physical_short'
+                + f'_long_data_tau_{tau}_tau_p_{tau_p}/taq_self_response_year'
+                + f'_responses_physical_short_long_data_tau_{tau}_tau'
+                + f'_p_{tau_p}_{year}_{ticker_i}.pickle', 'rb'))
+
+            # Addition of the short and long response signal
+            sum = np.zeros(tau)
+            sum[:tau_p + 1] = self_short[:tau_p + 1]
+            sum[tau_p + 1:] = self_short[tau_p + 1:] + self_long[tau_p + 1:]
+
+            ax1.semilogx(self_short, linewidth=5, label='Short')
+            ax1.semilogx(self_long, linewidth=5, label='Long')
+            ax1.semilogx(sum, linewidth=5, label='Sum')
+            ax1.semilogx(self_response, linewidth=5, label='Self-response')
+            ax1.semilogx(self_shuffle, linewidth=5, label='Shuffle')
+            ax1.plot((tau_p, tau_p), (-1000, 1000), '--k', linewidth=5,
+                     label=r"$\tau' $ = {}".format(tau_p))
+
+            ax1.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2), ncol=3,
+                       fontsize=15)
+            ax1.set_xlabel(r'$\tau \, [s]$', fontsize=15)
+            ax1.set_ylabel(r'$R^{sl}_{ii}(\tau)$', fontsize=15)
+            ax1.tick_params(axis='x', labelsize=10)
+            ax1.tick_params(axis='y', labelsize=10)
+            ax1.set_xlim(1, 1000)
+            ax1.set_ylim(-0.2 * 10 ** -4, 1.6 * 10 ** -4)
+            ax1.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
+            ax1.yaxis.offsetText.set_fontsize(10)
+            ax1.grid(True)
+
+            # Load data cross-response
             (cross_short,
              cross_long,
              cross_response,
-             cross_shuffle) = pickle.load(open(''.join((
-                                          '../../project/taq_data/responses'
-                                          + '_time_short_long_data_{2}/taq'
-                                          + '_cross_response_year_time_short'
-                                          + '_long_tau_data_tau_{3}_tau_p_{4}'
-                                          + '/taq_cross_response_year_time'
-                                          + '_short_long_tau_data_tau_{3}_tau'
-                                          + '_p_{4}_{2}_{0}i_{1}j.pickle')
-                                          .split())
-                                          .format(ticker_i, ticker_j, year,
-                                                  tau, tau_p), 'rb'))
+             cross_shuffle) = pickle.load(open(
+                 f'../../project/taq_data/responses_physical_short_long_data'
+                 + f'_{year}/taq_cross_response_year_responses_physical_short'
+                 + f'_long_data_tau_{tau}_tau_p_{tau_p}/taq_cross_response'
+                 + f'_year_responses_physical_short_long_data_tau_{tau}_tau'
+                 + f'_p_{tau_p}_{year}_{ticker_i}i_{ticker_j}j.pickle', 'rb'))
 
             # Addition of the short and long response signal
             sum = np.zeros(tau)
             sum[:tau_p + 1] = cross_short[:tau_p + 1]
             sum[tau_p + 1:] = cross_short[tau_p + 1:] + cross_long[tau_p + 1:]
 
-            plt.semilogx(cross_short, linewidth=5, label='Short')
-            plt.semilogx(cross_long, linewidth=5, label='Long')
-            plt.semilogx(sum, linewidth=5, label='Sum')
-            plt.semilogx(cross_response, linewidth=5, label='Cross-response')
-            plt.semilogx(cross_shuffle, linewidth=5, label='Shuffle')
-            plt.plot((tau_p, tau_p), (-1000, 1000), '--k', linewidth=5,
+            ax2.semilogx(cross_short, linewidth=5, label='Short')
+            ax2.semilogx(cross_long, linewidth=5, label='Long')
+            ax2.semilogx(sum, linewidth=5, label='Sum')
+            ax2.semilogx(cross_response, linewidth=5, label='Cross-response')
+            ax2.semilogx(cross_shuffle, linewidth=5, label='Shuffle')
+            ax2.plot((tau_p, tau_p), (-1000, 1000), '--k', linewidth=5,
                      label=r"$\tau' $ = {}".format(tau_p))
 
-            plt.legend(loc='lower left', fontsize=35)
-            # plt.title('Cross-response', fontsize=40)
-            plt.xlabel(r'$\tau \, [s]$', fontsize=45)
-            plt.ylabel(r'$R_{ij}(\tau)$', fontsize=45)
-            plt.xticks(fontsize=35)
-            plt.yticks(fontsize=35)
-            plt.xlim(1, 1000)
-            plt.ylim(-8 * 10 ** -5, 9 * 10 ** -5)
-            plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
-            ax.yaxis.offsetText.set_fontsize(35)
-            plt.grid(True)
-            plt.tight_layout()
-            plt.show()
+            ax2.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2), ncol=3,
+                       fontsize=15)
+            ax2.set_xlabel(r'$\tau \, [s]$', fontsize=15)
+            ax2.set_ylabel(r'$R^{sl}_{ij}(\tau)$', fontsize=15)
+            ax2.tick_params(axis='x', labelsize=10)
+            ax2.tick_params(axis='y', labelsize=10)
+            ax2.set_xlim(1, 1000)
+            ax2.set_ylim(-5 * 10 ** -5, 9 * 10 ** -5)
+            ax2.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
+            ax2.yaxis.offsetText.set_fontsize(10)
+            ax2.grid(True)
 
-            # Plotting
-            figure.savefig('../plot/05_cross_short_long_{}_{}.png'
-                           .format(ticker_i, ticker_j))
+            plt.tight_layout()
+
+            # Save plot
+            figure.savefig(f'../plot/05_short_long_{ticker_i}_{ticker_j}.png')
 
             return None
 
@@ -173,10 +144,7 @@ def main():
 
     year = '2008'
 
-    taq_self_response_year_avg_responses_time_short_long_plot('AAPL', year,
-                                                              1000, 40)
-    taq_cross_response_year_avg_responses_time_short_long_plot('AAPL', 'MSFT',
-                                                               year, 1000, 40)
+    taq_responses_physical_short_long_year_plot('AAPL', 'MSFT', year, 1000, 50)
 
     return None
 
