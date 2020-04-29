1320984ea2f31b017bca26a7c69cc758b03b2a62
juanhenao21 <juanhenao21@gmail.com>
2020-04-29 12:27:00 +0200

    Update docstrings

--- project/taq_responses_physical/taq_algorithms/taq_data_tools_responses_physical.py
+++ project/taq_responses_physical/taq_algorithms/taq_data_tools_responses_physical.py
@@ -6,7 +6,6 @@ in the modules that use them.
 
 This script requires the following modules:
     * matplotlib
-    * numpy
     * os
     * pandas
     * pickle
@@ -18,7 +17,7 @@ The module contains the following functions:
     * taq_function_header_print_data - prints info about the function running.
     * taq_function_header_print_plot - prints info about the plot.
     * taq_start_folders - creates folders to save data and plots.
-    * taq_initial_data - takes the initial values for the analysis.
+    * taq_initial_message - prints the initial message with basic information.
     * taq_business_days - creates a list of week days for a year.
     * taq_decompress - decompress original data format to CSV file.
     * main - the main function of the script.
@@ -30,7 +29,6 @@ The module contains the following functions:
 # Modules
 
 from matplotlib import pyplot as plt
-import numpy as np
 import os
 import pandas as pd
 import pickle
@@ -172,7 +170,7 @@ def taq_function_header_print_data(function_name, ticker_i, ticker_j, year,
     # Self-response data
     else:
         print(f'Processing data for the stock {ticker_i} the '
-              + f'{year}.{month}.{day}')
+              + f'{year}.{month}.{day}.')
 
     return None
 
@@ -205,7 +203,7 @@ def taq_function_header_print_plot(function_name, ticker_i, ticker_j, year,
     # Self-response data
     else:
         print(f'Processing plot for the stock {ticker_i} the '
-              + f'{year}.{month}.{day}')
+              + f'{year}.{month}.{day}.')
 
     return None
 
@@ -220,85 +218,48 @@ def taq_start_folders(year):
     """
 
     try:
-        os.mkdir('../../taq_plot/')
         os.mkdir(f'../../taq_plot/responses_physical_plot_{year}')
-        os.mkdir('../../taq_data/')
         os.mkdir(f'../../taq_data/responses_physical_data_{year}')
-        os.mkdir(f'../../taq_data/original_year_data_{year}')
 
         print('Folder to save data created')
         print()
 
-        res = 'no'
-        while (res == 'no'):
-            print('Please move the .quotes and .trades files to the '
-                  + 'original_year_data_2008 folder and move the '
-                  + 'decompress_original_data_2008 folder to the taq_data '
-                  + 'folder ...')
-            print()
-            print('Or')
-            print()
-            print(f'Move the CSV year data files to the '
-                  + f'csv_year_data_{year} folder')
-            print()
-            print('Are you ready to continue? (yes/no): ')
-            res = input()
-            print()
-
     except FileExistsError as e:
         print('Folder exists. The folder was not created')
         print(e)
-        # raise Exception('Check the folders')
 
     return None
 
 # -----------------------------------------------------------------------------
 
 
-def taq_initial_data():
-    """Takes the initial values for the analysis
+def taq_initial_message():
+    """Prints the initial message with basic information.
 
-    :return: Tuple -- The function return a tuple with a string with the year
-     to be analyzed and a list with the name of the tickers.
+    :return: None -- The function prints a message and does not return a value.
     """
 
     print()
-    print('#########################################')
-    print('Response Functions Physical Time Analysis')
-    print('#########################################')
+    print('###############################################')
+    print('Price Response Functions Physical Time Analysis')
+    print('###############################################')
     print('AG Guhr')
     print('Faculty of Physics')
     print('University of Duisburg-Essen')
     print('Author: Juan Camilo Henao Londono')
     print('More information in:')
-    print('  * https://juanhenao21.github.io/')
-    print('  * https://github.com/juanhenao21/response_functions_year')
-    print('  * https://response-functions-year.readthedocs.io/en/latest/')
+    print('* https://juanhenao21.github.io/')
+    print('* https://github.com/juanhenao21/financial_response_spread_year')
+    print('* https://financial-response-spread-year.readthedocs.io/en/latest/')
     print()
 
-    print('How many tickers do you want to analyze?')
-    n_tick = int(input())
-    tickers = []
-
-    for _ in range(n_tick):
-
-        print(f'Insert the symbol of the ticker (i.e. GOOG):')
-        res = input()
-
-        tickers.append(res)
-
-    print()
-    print('Please enter the year to be analyzed (i.e. 2008): ')
-    year = input()
-    print()
-
-    return (year, tickers)
+    return None
 
 # -----------------------------------------------------------------------------
 
 
 def taq_bussiness_days(year):
-    """Generates a list with the dates of the bussiness days in a year
+    """Generates a list with the dates of the bussiness days in a year.
 
     :param year: string of the year to be analyzed (i.e '2008').
     :return: list.
