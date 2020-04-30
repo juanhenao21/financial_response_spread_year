7334f51896074e846d98df6ccbb1476902363e22
juanhenao21 <juanhenao21@gmail.com>
2020-04-30 10:36:57 +0200

    Update doctstrings

--- project/taq_responses_physical_shift/taq_algorithms/taq_data_plot_responses_physical_shift.py
+++ project/taq_responses_physical_shift/taq_algorithms/taq_data_plot_responses_physical_shift.py
@@ -5,7 +5,6 @@ taq_data_analysis_responses_physical_shift module.
 
 This script requires the following modules:
     * matplotlib
-    * numpy
     * pickle
     * taq_data_tools_response_physical_shift
 
@@ -24,7 +23,6 @@ The module contains the following functions:
 
 
 from matplotlib import pyplot as plt
-import numpy as np
 import pickle
 
 import taq_data_tools_responses_physical_shift
